430759d84a15a82776a50d2682a266a8dfc1d202
Phoeningurix <159692676+Phoeningurix@users.noreply.github.com>
2025-05-21 22:11:20 +0200

    change design
    - change button design
    - change list design
    - add navbar
    - add default images

--- code/frontend/IbraPloe/src/App.tsx
+++ code/frontend/IbraPloe/src/App.tsx
@@ -25,7 +25,7 @@ const App: React.FC = () => {
   }, []);
 
   return (
-    <div>
+    <div className="bg-info-subtle">
       <Header />
       <main>
         <h2>Gefundene Fahrrad-Incidents</h2>
--- code/frontend/IbraPloe/src/components/LocationList.tsx
+++ code/frontend/IbraPloe/src/components/LocationList.tsx
@@ -8,14 +8,35 @@ interface LocationListProps {
 
 const LocationList: React.FC<LocationListProps> = ({ locations }) => {
   return (
-    <div>
+    <div className="d-flex flex-column align-items-center px-3">
       <h2>Locations</h2>
       <ul>
         {locations.map((location) => (
-          <li key={location.incident_id}>
-            <h3>{location.title}</h3>
-            <p>{location.description}</p>
-            <Counter/>
+          <li className="list-unstyled" key={location.incident_id}>
+            <div className="card m-2 m-md-4">
+              <div className="row">
+                <div className="col-7">
+                  <h3>{location.title}</h3>
+                  <p className="text-start">{'Description: ' + (location.description.length < 1 ? '-' : location.description)}</p>
+                  <p className="text-start">{'Street: ' + location.street}</p>
+                  <p className="text-start">{'Zip & City: ' + (location.zip + ' ' + location.city)}</p>
+                  <p className="text-start">{'Category: ' + location.category}</p>
+                  <p className="text-start">{'Image: ' + (location.images[0] == null ? '-' : location.images[0].image)}</p>
+                  <p className="text-start">{'Image count: ' + location.images.length}</p>
+                </div>
+
+                <div className="col-4">
+                  <img
+                    src= 'src\assets\Bike_default.PNG'
+                    className="img-thumbnail w-50"
+                    alt={location.title}
+                  />
+                </div>
+                <div className="col-1">
+                  <Counter/>
+                </div>
+              </div>
+            </div>
           </li>
         ))}
       </ul>
